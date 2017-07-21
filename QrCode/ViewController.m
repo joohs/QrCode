12a2eac3717ccf0644b50062a75d26a10061394a
xiangxiong <wuxiangxiong@bertadata.com>
2017-07-21 09:56:51 +0800

    Initial Commit

---
+++ QrCode/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  QrCode
+//
+//  Created by xiangxiong on 2017/7/21.
+//  Copyright © 2017年 xiangxiong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
